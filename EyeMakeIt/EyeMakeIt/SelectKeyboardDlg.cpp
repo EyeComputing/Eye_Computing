fdee9380577cbaece5bdcf3e95a630ee5b1ddf92
unknown <>
2016-07-28 16:44:23 +0900

    한글 버튼 추가

--- EyeMakeIt/EyeMakeIt/SelectKeyboardDlg.cpp
+++ EyeMakeIt/EyeMakeIt/SelectKeyboardDlg.cpp
@@ -40,6 +40,35 @@ void SelectKeyboardDlg::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_S_ENT, s_btn_ent);
 	DDX_Control(pDX, IDC_S_SPE, s_btn_spe);
 	DDX_Control(pDX, IDC_S_CON, s_btn_con);
+	DDX_Control(pDX, IDC_K_BIE, k_btn_bie);
+	DDX_Control(pDX, IDC_K_JIE, k_btn_jie);
+	DDX_Control(pDX, IDC_K_DIG, k_btn_dig);
+	DDX_Control(pDX, IDC_K_GIY, k_btn_giy);
+	DDX_Control(pDX, IDC_K_SHI, k_btn_shi);
+	DDX_Control(pDX, IDC_K_YYO, k_btn_yyo);
+	DDX_Control(pDX, IDC_K_YEO, k_btn_yeo);
+	DDX_Control(pDX, IDC_K_YYA, k_btn_yya);
+	DDX_Control(pDX, IDC_K_AEE, k_btn_aee);
+	DDX_Control(pDX, IDC_K_EEE, k_btn_eee);
+	DDX_Control(pDX, IDC_K_MIE, k_btn_mie);
+	DDX_Control(pDX, IDC_K_NIE, k_btn_nie);
+	DDX_Control(pDX, IDC_K_IEU, k_btn_ieu);
+	DDX_Control(pDX, IDC_K_RIE, k_btn_rie);
+	DDX_Control(pDX, IDC_K_HIE, k_btn_hie);
+	DDX_Control(pDX, IDC_K_OOO, k_btn_ooo);
+	DDX_Control(pDX, IDC_K_EOO, k_btn_eoo);
+	DDX_Control(pDX, IDC_K_AAA, k_btn_aaa);
+	DDX_Control(pDX, IDC_K_YII, k_btn_yii);
+	DDX_Control(pDX, IDC_S_KNG, s_btn_kng);
+	DDX_Control(pDX, IDC_K_KIE, k_btn_kie);
+	DDX_Control(pDX, IDC_K_TIE, k_btn_tie);
+	DDX_Control(pDX, IDC_K_CHI, k_btn_chi);
+	DDX_Control(pDX, IDC_K_PIE, k_btn_pie);
+	DDX_Control(pDX, IDC_S_SPC, s_btn_spc);
+	DDX_Control(pDX, IDC_K_YUU, k_btn_yuu);
+	DDX_Control(pDX, IDC_K_UUU, k_btn_uuu);
+	DDX_Control(pDX, IDC_K_EUU, k_btn_euu);
+	DDX_Control(pDX, IDC_S_DOT, s_btn_dot);
 }
 
 
@@ -78,26 +107,66 @@ BOOL SelectKeyboardDlg::OnInitDialog()
 	n_btn_zer.SetSkin(IDB_N_ZER, IDB_N_ZER, IDB_N_ZER_OVER, 0, 0, IDB_MASK, 1, 0, 4);
 
 	// 시스템 버튼 스킨 씌우기
-	s_btn_bck.SetSkin(IDB_S_BCK, IDB_S_BCK, IDB_S_BCK_OVER, 0, 0, IDB_MASK, 1, 0, 4);;
-	s_btn_shf.SetSkin(IDB_S_SHF, IDB_S_SHF, IDB_S_SHF_OVER, 0, 0, IDB_MASK, 1, 0, 4);;
-	s_btn_bks.SetSkin(IDB_S_BKS, IDB_S_BKS, IDB_S_BKS_OVER, 0, 0, IDB_MASK, 1, 0, 4);;
-	s_btn_ent.SetSkin(IDB_S_ENT, IDB_S_ENT, IDB_S_ENT_OVER, 0, 0, IDB_MASK, 1, 0, 4);;
-	s_btn_spe.SetSkin(IDB_S_SPE, IDB_S_SPE, IDB_S_SPE_OVER, 0, 0, IDB_MASK, 1, 0, 4);;
-	s_btn_con.SetSkin(IDB_S_CON, IDB_S_CON, IDB_S_CON_OVER, 0, 0, IDB_MASK, 1, 0, 4);;
-	
-	// 숫자 버튼 좌표, 크기 설정
-	for (int i = 0; i < 10; i++)
-	{
-		GetDlgItem(2000 + i)->SetWindowPos(NULL, ButtonSize.cx * i, 0, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
-	}
+	s_btn_bck.SetSkin(IDB_S_BCK, IDB_S_BCK, IDB_S_BCK_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	s_btn_shf.SetSkin(IDB_S_SHF, IDB_S_SHF, IDB_S_SHF_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	s_btn_bks.SetSkin(IDB_S_BKS, IDB_S_BKS, IDB_S_BKS_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	s_btn_ent.SetSkin(IDB_S_ENT, IDB_S_ENT, IDB_S_ENT_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	s_btn_spe.SetSkin(IDB_S_SPE, IDB_S_SPE, IDB_S_SPE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	s_btn_con.SetSkin(IDB_S_CON, IDB_S_CON, IDB_S_CON_OVER, 0, 0, IDB_MASK, 1, 0, 4);
 
-	// 시스템 버튼 좌표, 크기 설정
-	GetDlgItem(IDC_S_BCK)->SetWindowPos(NULL, ButtonSize.cx * 0, ButtonSize.cy * 1, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
-	GetDlgItem(IDC_S_SHF)->SetWindowPos(NULL, ButtonSize.cx * 0, ButtonSize.cy * 2, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
-	GetDlgItem(IDC_S_BKS)->SetWindowPos(NULL, ButtonSize.cx * 8, ButtonSize.cy * 1, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
-	GetDlgItem(IDC_S_ENT)->SetWindowPos(NULL, ButtonSize.cx * 8, ButtonSize.cy * 2, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
-	GetDlgItem(IDC_S_SPE)->SetWindowPos(NULL, ButtonSize.cx * 9, ButtonSize.cy * 1, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
-	GetDlgItem(IDC_S_CON)->SetWindowPos(NULL, ButtonSize.cx * 9, ButtonSize.cy * 2, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
+	s_btn_kng.SetSkin(IDB_S_KNG, IDB_S_KNG, IDB_S_KNG_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	s_btn_spc.SetSkin(IDB_S_SPC, IDB_S_SPC, IDB_S_SPC_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	s_btn_dot.SetSkin(IDB_S_DOT, IDB_S_DOT, IDB_S_DOT_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+
+	// 한글 버튼 스킨 씌우기
+	k_btn_bie.SetSkin(IDB_K_BIE, IDB_K_BIE, IDB_K_BIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_jie.SetSkin(IDB_K_JIE, IDB_K_JIE, IDB_K_JIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_dig.SetSkin(IDB_K_DIG, IDB_K_DIG, IDB_K_DIG_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_giy.SetSkin(IDB_K_GIY, IDB_K_GIY, IDB_K_GIY_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_shi.SetSkin(IDB_K_SHI, IDB_K_SHI, IDB_K_SHI_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_yyo.SetSkin(IDB_K_YYO, IDB_K_YYO, IDB_K_YYO_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_yeo.SetSkin(IDB_K_YEO, IDB_K_YEO, IDB_K_YEO_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_yya.SetSkin(IDB_K_YYA, IDB_K_YYA, IDB_K_YYA_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_aee.SetSkin(IDB_K_AEE, IDB_K_AEE, IDB_K_AEE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_eee.SetSkin(IDB_K_EEE, IDB_K_EEE, IDB_K_EEE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_mie.SetSkin(IDB_K_MIE, IDB_K_MIE, IDB_K_MIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_nie.SetSkin(IDB_K_NIE, IDB_K_NIE, IDB_K_NIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_ieu.SetSkin(IDB_K_IEU, IDB_K_IEU, IDB_K_IEU_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_rie.SetSkin(IDB_K_RIE, IDB_K_RIE, IDB_K_RIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_hie.SetSkin(IDB_K_HIE, IDB_K_HIE, IDB_K_HIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_ooo.SetSkin(IDB_K_OOO, IDB_K_OOO, IDB_K_OOO_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_eoo.SetSkin(IDB_K_EOO, IDB_K_EOO, IDB_K_EOO_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_aaa.SetSkin(IDB_K_AAA, IDB_K_AAA, IDB_K_AAA_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_yii.SetSkin(IDB_K_YII, IDB_K_YII, IDB_K_YII_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_kie.SetSkin(IDB_K_KIE, IDB_K_KIE, IDB_K_KIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_tie.SetSkin(IDB_K_TIE, IDB_K_TIE, IDB_K_TIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_chi.SetSkin(IDB_K_CHI, IDB_K_CHI, IDB_K_CHI_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_pie.SetSkin(IDB_K_PIE, IDB_K_PIE, IDB_K_PIE_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_yuu.SetSkin(IDB_K_YUU, IDB_K_YUU, IDB_K_YUU_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_uuu.SetSkin(IDB_K_UUU, IDB_K_UUU, IDB_K_UUU_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+	k_btn_euu.SetSkin(IDB_K_EUU, IDB_K_EUU, IDB_K_EUU_OVER, 0, 0, IDB_MASK, 1, 0, 4);
+
+	
+	// 버튼 좌표 및 크기 설정
+	for (int y = 0; y < 6; y++)
+		for (int x = 0; x < 10; x++)
+		{
+			// 리소스 순서를 받기 위한 변수
+			int order = 2000 + (10 * y) + x;
+
+			// 대화상자 부분은 생략
+			if (order > 2010 && order < 2018)	continue;
+			else if (order > 2020 && order < 2028)	continue;
+
+			// 스페이스 처리
+			else if (order == 2054)
+				GetDlgItem(order)->SetWindowPos(NULL, ButtonSize.cx * x, ButtonSize.cy * y, ButtonSize.cx * 2, ButtonSize.cy, SWP_NOZORDER);
+			else if (order == 2055)	continue;
+
+			// 예외사항 아닐 경우 x, y 좌표에 버튼 크기 만큼 출력
+			else
+				GetDlgItem(order)->SetWindowPos(NULL, ButtonSize.cx * x, ButtonSize.cy * y, ButtonSize.cx, ButtonSize.cy, SWP_NOZORDER);
+		}
 
 	return TRUE;  // return TRUE unless you set the focus to a control
 				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
