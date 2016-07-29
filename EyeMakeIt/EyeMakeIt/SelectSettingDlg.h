df31053c1e300d9500731c982d9377935001af7e
unknown <>
2016-07-29 20:26:24 +0900

    응시점 설정 버튼 추가

--- EyeMakeIt/EyeMakeIt/SelectSettingDlg.h
+++ EyeMakeIt/EyeMakeIt/SelectSettingDlg.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "resource.h"
+#include "xSkinButton.h"
 
 // SelectSettingDlg 대화 상자입니다.
 
@@ -15,6 +16,10 @@ public:
 	/* 사용자 정의 함수 */
 	void SelectSettingDlg::OnBtnClick(UINT uiID);
 
+	// 윈도우 사이즈와 버튼 사이즈 값을 입력받기 위한 변수
+	SIZE WindowSize;
+	SIZE ButtonSize;
+
 // 대화 상자 데이터입니다.
 	enum { IDD = IDD_Dlg_Setting };
 
@@ -22,4 +27,7 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
 
 	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	CxSkinButton m_btn_eye;
 };
